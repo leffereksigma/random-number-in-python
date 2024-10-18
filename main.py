eb99c2fadbd2b78f763e0ebbb7084b4dd613992e
lefferek <154812446+leffereksigma@users.noreply.github.com>
2024-10-18 11:31:20 +0200

    main.py

---
+++ main.py
@@ -0,0 +1,11 @@
+import random
+import os
+
+print(" ")
+print("made by leffereksigma (https://github.com/leffereksigma)")
+print("skid project frfr")
+print(" ")
+
+for i in range (100):
+    print(random.randrange(1000, 9999))
+os.system('pause >nul')
